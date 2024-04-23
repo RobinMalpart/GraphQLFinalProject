6b34ab9d7ed8762c6f5b36684955a86376363e37
robin.malpart <malpart.robin@gmail.com>
2024-04-23 15:59:02 +0200

    update resolver.ts and resolver.ts files

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,22 +1,17 @@
 import { GraphQLError } from "graphql";
-import { Resolvers, Speciality } from "./types.js";
+import { Resolvers } from "./types.js";
 import { createUser } from "./mutations/user/createUser.js";
 import { signIn } from "./mutations/user/signIn.js";
 
-
 export const resolvers: Resolvers = {
   Query: {
-
     multiply: (parent, args, context, info) => {
-      const {number1, number2} = args
-      return number1 * number2
+      const { number1, number2 } = args;
+      return number1 * number2;
     },
-
   },
   Mutation: {
-   
     createUser,
     signIn,
   },
-
- };
+};
