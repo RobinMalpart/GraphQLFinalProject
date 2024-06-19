3aa997964a953a0aefc72857c24dedbe7aa6c2d6
Julien-Somasundaram <somasundaramjulien@gmail.com>
2024-06-19 09:46:59 +0200

    ajout des champs pour les likes et les commentaires

--- src/mutations/comment/createComment.ts
+++ src/mutations/comment/createComment.ts
@@ -1,45 +1,54 @@
-import { print } from "graphql";
 import { MutationResolvers } from "../../types.js";
 
-export const createComment: MutationResolvers['createComment'] = async (_, {content, userId, articleId}, {dataSources}) => {
+export const createComment: MutationResolvers['createComment'] = async (_, { content, userId, articleId }, { dataSources }) => {
   try {
-    const createdcomment = await dataSources.db.comment.create({
+    const createdComment = await dataSources.db.comment.create({
       data: {
         content,
         User: {
-            connect: {
-                id: userId
-            }
+          connect: {
+            id: userId,
           },
+        },
         Article: {
-            connect: {
-                id: articleId
-            } 
+          connect: {
+            id: articleId,
+          },
+        },
       },
-    },
-        include: {
-            User: true,
-            Article: true
-        }
-    })
+      include: {
+        User: true,
+        Article: true,
+      },
+    });
 
     return {
       code: 201,
       message: 'Comment has been created',
       success: true,
       comment: {
-        id: createdcomment.id,
-        content: createdcomment.content
-      }
-
-    }
-  } catch(e) {
+        id: createdComment.id,
+        content: createdComment.content,
+        userId: createdComment.userId,
+        articleId: createdComment.articleId,
+        User: {
+          id: createdComment.User.id,
+          username: createdComment.User.username,
+        },
+        Article: {
+          id: createdComment.Article.id,
+          title: createdComment.Article.title,
+          content: createdComment.Article.content,
+        },
+      },
+    };
+  } catch (e) {
     console.error('Error creating comment:', e);
     return {
       code: 403,
       message: 'Comment has not been created',
       success: false,
       comment: null,
-    }
+    };
   }
-}
+};
