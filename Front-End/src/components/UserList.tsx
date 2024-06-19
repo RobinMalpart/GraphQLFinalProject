12601670adfe6423c71f74b4f8e2a908da95932e
Flamby <66632621+Kaeggame@users.noreply.github.com>
2024-06-19 19:34:46 +0200

    Verif final

--- Front-End/src/components/UserList.tsx
+++ Front-End/src/components/UserList.tsx
@@ -6,7 +6,6 @@ import { GetUsersQuery } from '../generated/graphql';
 const UserList: React.FC = () => {
   const { loading, error, data } = useQuery<GetUsersQuery>(GET_USERS);
 
-
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
@@ -15,7 +14,7 @@ const UserList: React.FC = () => {
     return colors[Math.floor(Math.random() * colors.length)];
   };
 
-  function shuffleArray(array: any[]) {
+  function shuffleArray<T>(array: T[]): T[] {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
@@ -25,13 +24,13 @@ const UserList: React.FC = () => {
 
   return (
     <ul>
-        {shuffleArray(data?.getUsers.slice(0, 10)).map(user => (
+      {shuffleArray(data?.getUsers.slice(0, 10) ?? []).map(user => (
         <li key={user.id} className="flex my-1 items-center relative">
           <img src='https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg' alt="Profile" className="h-8 w-8 object-cover mr-3 rounded-xl" />
           <span className={`h-3 w-3 rounded-full absolute bottom-0 left-0 ${getRandomColor()}`}></span>
           <span>{user.username}</span>
         </li>
-        ))}
+      ))}
     </ul>
   );
 };
